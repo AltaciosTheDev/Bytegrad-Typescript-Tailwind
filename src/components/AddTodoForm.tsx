c46baaed4e2348803dc4044a9efff991269d91e4
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-01-04 17:47:00 -0600

    button props

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -6,7 +6,7 @@ export default function AddTodoForm() {
     <form >
         <h2 className='font-medium text-[#231d15]'>Add Todo</h2>
         <input type='text' className='h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] block w-full px-[16px]'/>
-        <Button/>
+        <Button>Add to list</Button>
     </form>
   )
 }
