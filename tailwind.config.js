aa7baebf9c89db10065f5148af0fa5544b78d991
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-01-04 13:01:54 -0600

    setup and install

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",               // Include the `index.html` file at the root.
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all files in the `src` directory and its subdirectories 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
