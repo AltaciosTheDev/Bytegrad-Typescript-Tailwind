e976465a298179cb50dade9521be834b7a12c596
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-01-05 17:08:20 -0600

    submit todo feature

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import Button from './Button'
+import React, { useState } from "react";
+import Button from "./Button";
 
-export default function AddTodoForm() {
+export default function AddTodoForm({todos, setTodos}) {
+  const [todoText, setTodoText] = useState('')
+
+  const handleChange = (e)=> {
+    setTodoText(e.target.value)
+  }
+
   return (
-    <form >
-        <h2 className='font-medium text-[#231d15]'>Add Todo</h2>
-        <input type='text' className='h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] block w-full px-[16px]'/>
-        <Button>Add to list</Button>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setTodos(prev => [
+          ...prev, {id:prev.length + 1, text: todoText, isCompleted: false}
+          
+        ])
+        setTodoText("")
+      }}
+    >
+      <h2 className="font-medium text-[#231d15]">Add Todo</h2>
+      <input
+        type="text"
+        className="h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] block w-full px-[16px]"
+        value={todoText}
+        onChange={handleChange}
+      />
+      <Button>Add to list</Button>
     </form>
-  )
+  );
 }
