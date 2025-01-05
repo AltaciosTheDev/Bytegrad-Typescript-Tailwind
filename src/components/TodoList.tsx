d8ddc723527eec285b9eac696b77115d362fcaf7
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-01-04 18:09:30 -0600

    todo list markup

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,20 @@
+import DeleteButton from "./DeleteButton";
+
+const initialTodos = ["buy groceries", "walk the dog", "do laundry", "study"];
+
 export default function TodoList() {
   return (
-    <ul></ul>
-  )
+    <ul>
+      {initialTodos.map((todo) => {
+        return (
+          <li
+            key={todo}
+            className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor pointer border-b border-black/[8%]"
+          >
+            <span>{todo}</span> <DeleteButton/>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
