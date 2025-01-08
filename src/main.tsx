f08c24af3844c1e447fb4d2e73c512fe856e667e
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-01-07 21:01:14 -0600

    fixed url

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,8 @@ createRoot(document.getElementById("root")!).render(
     <KindeProvider
       clientId="8ff2dd278a384289a457a0db840c1839"
       domain="https://altacios.kinde.com"
-      redirectUri={process.env.NODE_ENV === "production" ? "https://bytegrad-typescript-tailwind-xr8xtfxh6-altacios-projects.vercel.app" : "http://localhost:5173"}
-      logoutUri={process.env.NODE_ENV === "production" ? "https://bytegrad-typescript-tailwind-xr8xtfxh6-altacios-projects.vercel.app" : "http://localhost:5173"}
+      redirectUri={process.env.NODE_ENV === "production" ? "https://bytegrad-typescript-tailwind.vercel.app/" : "http://localhost:5173"}
+      logoutUri={process.env.NODE_ENV === "production" ? "https://bytegrad-typescript-tailwind.vercel.app/" : "http://localhost:5173"}
     >
       <TodosContextProvider>
         <App />
