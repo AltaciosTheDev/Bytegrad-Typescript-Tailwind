bd388904f858b7a018e1fe8f1464b7a7b5addbd6
AltaciosTheDev <enzo.altamirano98@gmail.com>
2025-01-07 20:19:46 -0600

    auth for login,logout,register,user info, isAuthed, isLoading from kinde

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
 type ButtonProps = {
+  onClick?: () => Promise<void> //if type is too problematic, just remove the parameters
   buttonType?: "primary" | "secondary",
   children: React.ReactNode
 }
 
-export default function Button({buttonType, children}: ButtonProps) {
+export default function Button({onClick, buttonType, children}: ButtonProps) {
   
   return (
     <button
+      onClick={onClick}
       className={`h-[45px] bg-[#473a2b] hover:bg-[#322618] w-full text-white rounded-[5px] cursor-pointer ${
         buttonType === "secondary" ? "opacity-[85%]" : ""
       }`}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App.tsx";
 import TodosContextProvider from "./contexts/TodosContextProvider.tsx";
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TodosContextProvider>
-      <App />
-    </TodosContextProvider>
+    <KindeProvider
+      clientId="8ff2dd278a384289a457a0db840c1839"
+      domain="https://altacios.kinde.com"
+      redirectUri="http://localhost:5173"
+      logoutUri="http://localhost:5173"
+    >
+      <TodosContextProvider>
+        <App />
+      </TodosContextProvider>
+    </KindeProvider>
   </StrictMode>
 );
